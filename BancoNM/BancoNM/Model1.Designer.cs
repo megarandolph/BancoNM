88bf381c8f464c338a097b2b06ae4933f8792158
megarandolph <randolphmarte@hotmail.com>
2020-03-30 22:05:13 -0400

    Todos los mantenimientos creados.

--- BancoNM/BancoNM/Model1.Designer.cs
+++ BancoNM/BancoNM/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'E:\ITLA\Materias\Administracion de proyectos de software\Final\BancoNM\BancoNM\Model1.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\rando\source\repos\megarandolph\BancoNM\BancoNM\BancoNM\Model1.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
